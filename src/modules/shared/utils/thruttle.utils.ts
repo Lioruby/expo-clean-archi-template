ef0b425ee943acda6a019ab35a1424506b2c2f3b
Lior Levy <lioruby@macbook-pro.home>
2024-08-09 21:01:12 +0200

    chore: remove thruttle function

--- src/modules/shared/utils/thruttle.utils.ts
+++
@@ -1,9 +0,0 @@
-export const thruttle = (fn: Function, delay: number) => {
-  let timer: NodeJS.Timeout;
-  return function (this: any, ...args: any[]) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      fn.apply(this, args);
-    }, delay);
-  };
-};
